a554f4a39f66a44253f05e6698deb0721008c838
Ali Khaled <alii.khaled97@gmail.com>
2020-12-25 22:00:49 +0200

    Added SystemTest cases

--- src/System_Test.java
+++ src/System_Test.java
@@ -8,10 +8,6 @@ import java.io.PrintStream;
 
 public class System_Test {
     Menu menu;
-    // All functions in main:
-    // add customer(D)                  display empty rooms(D)                  find rooms(D)
-    // delete customer(D)               view rooms(D)                           initialize(D)
-    // retrieve data                    alphabetical order                      store data
 
     @Test
     void systemTestCase1(){
@@ -98,7 +94,7 @@ public class System_Test {
 
     @Test
     void systemTestCase3(){
-        System.out.println("This is system test case #2");
+        System.out.println("This is system test case #3");
 
         ByteArrayInputStream in = new ByteArrayInputStream("V A 1 Maged V A 1 Sameer V D 5 v f Ahmed f Ali q".getBytes());
         System.setIn(in);
@@ -134,5 +130,67 @@ public class System_Test {
         menu.menu();
         Assert.assertFalse(menu.findRoom("Ahmed"));
         Assert.assertTrue(menu.findRoom("Maged"));
+    }
+
+    @Test
+    void systemTestCase4(){
+        System.out.println("This is system test case #4");
+
+        ByteArrayInputStream in = new ByteArrayInputStream("V A 3 Maged V A 5 Sameer V A 4 Said D 5 v f Maged f Sameer q".getBytes());
+        System.setIn(in);
+
+        menu = new Menu();
+        menu.initialize();
+        menu.menu();
+        PrintStream oldOut = System.out;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        // Change System.out to point out to our stream
+        System.setOut(new PrintStream(baos));
+        menu.displayEmptyRooms();
+        System.setOut(oldOut);
+        String output = baos.toString();
+
+        for (int x = 0; x < 10; x++) {
+            if (menu.myHotel[x].getName().equals("e")) {
+                Assert.assertTrue(output.contains("room " + x + " is empty"));
+            }
+        }
+        menu.menu();
+        menu.menu();
+        menu.menu();
+        String [] rooms = menu.viewRooms();
+        for (int i=0; i<10; i++){
+            if (i == 3){
+                Assert.assertEquals(rooms[i], "Maged");
+            }
+            else if(i == 4){
+                Assert.assertEquals(rooms[i], "Said");
+            }
+            else{
+                Assert.assertEquals(rooms[i], "empty");
+            }
+        }
+        menu.menu();
+        menu.menu();
+        Assert.assertFalse(menu.findRoom("Ahmed"));
+        Assert.assertTrue(menu.findRoom("Maged"));
+        menu.storeData();
+
+        menu = new Menu();
+        menu.initialize();
+
+        menu.retrieveData();
+        rooms = menu.viewRooms();
+        for (int i=0; i<10; i++){
+            if (i == 3){
+                Assert.assertEquals(rooms[i], "Maged");
+            }
+            else if(i == 4){
+                Assert.assertEquals(rooms[i], "Said");
+            }
+            else{
+                Assert.assertEquals(rooms[i], "empty");
+            }
+        }
     }
 }
